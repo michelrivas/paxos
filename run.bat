e1a58d56a5f7424d7f9fd4337b206b1de40bfdc5
Michel Rivas Hernandez <M.Rivas1@uni.brighton.ac.uk>
2016-07-25 10:53:03 +0100

    Changing Windows command line cluster initializer

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-start dist\build\paxos\paxos.exe 5555 6666 7777
-start dist\build\paxos\paxos.exe 6666 7777
-start dist\build\paxos\paxos.exe 7777
+start dist\build\paxos\paxos.exe localhost:5555 localhost:6666 localhost:7777
+start dist\build\paxos\paxos.exe localhost:6666 localhost:7777
+start dist\build\paxos\paxos.exe localhost:7777
